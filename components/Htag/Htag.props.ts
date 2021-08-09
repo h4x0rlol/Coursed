9f5870920357914b6994a7b48f630cd4e89e5541
h4x0rlol <mazafaker.com@gmail.com>
2021-08-09 21:00:10 +0600

    Add button component

--- components/Htag/Htag.props.ts
+++ components/Htag/Htag.props.ts
@@ -2,7 +2,5 @@ import { ReactNode } from 'react';
 
 export interface HtagProps {
 	tag: 'h1' | 'h2' | 'h3';
-
-	
 	children: ReactNode;
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,15 @@
-import { Htag } from '../components';
+import { Button, Htag } from '../components';
 
 export default function Home(): JSX.Element {
-	return <Htag tag="h1">Text</Htag>;
+	return (
+		<>
+			<Htag tag="h1">Text</Htag>
+			<Button appearance="primary" arrow="right">
+				Button
+			</Button>
+			<Button appearance="ghost" arrow="right">
+				Button
+			</Button>
+		</>
+	);
 }
