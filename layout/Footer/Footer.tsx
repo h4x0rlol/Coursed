fd372745e1df57c5cc001812e6ebed395c4172ad
h4x0rlol <mazafaker.com@gmail.com>
2021-08-12 20:11:09 +0600

    Footer styles

--- layout/Footer/Footer.tsx
+++ layout/Footer/Footer.tsx
@@ -1,7 +1,17 @@
 import { FooterProps } from './Footer.props';
 import styles from './Footer.module.css';
 import cn from 'classnames';
+import { format } from 'date-fns';
 
-export const Footer = ({ ...props }: FooterProps): JSX.Element => {
-	return <div {...props}>Footer</div>;
+export const Footer = ({ className, ...props }: FooterProps): JSX.Element => {
+	return (
+		<footer className={cn(className, styles.footer)} {...props}>
+			<div>
+				Coursed &#169; 2021 &ndash; {format(new Date(), 'yyyy')} Все права
+				защищены
+			</div>
+			<a href="">Пользовательское соглашение</a>
+			<a href="">Политика конфиденциальности</a>
+		</footer>
+	);
 };
