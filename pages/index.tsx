ca37e607e46c85b7569e4412f444abb1949f22bf
h4x0rlol <mazafaker.com@gmail.com>
2021-08-18 17:32:32 +0600

    Implement SSR and some interfaces

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,10 @@
+import axios from 'axios';
+import { GetStaticProps } from 'next';
 import { Button, Htag, P, Rating, Tag } from '../components';
+import { MenuItem } from '../interfaces/menu.interface';
 import { withLayout } from '../layout/Layout';
 
-function Home(): JSX.Element {
+function Home({ menu }: HomeProps): JSX.Element {
 	return (
 		<>
 			<Htag tag="h1">Text</Htag>
@@ -19,8 +22,34 @@ function Home(): JSX.Element {
 				red
 			</Tag>
 			<Rating rating={4} isEditable />
+			<ul>
+				{menu.map((m) => (
+					<li key={m._id.secondCategory}>{m._id.secondCategory}</li>
+				))}
+			</ul>
 		</>
 	);
 }
 
 export default withLayout(Home);
+
+export const getStaticProps: GetStaticProps<HomeProps> = async () => {
+	const firstCategory = 0;
+	const { data: menu } = await axios.post<MenuItem[]>(
+		process.env.NEXT_PUBLIC_DOMAIN + '/api/top-page/find',
+		{
+			firstCategory,
+		}
+	);
+	return {
+		props: {
+			menu,
+			firstCategory,
+		},
+	};
+};
+
+interface HomeProps extends Record<string, unknown> {
+	menu: MenuItem[];
+	firstCategory: number;
+}
